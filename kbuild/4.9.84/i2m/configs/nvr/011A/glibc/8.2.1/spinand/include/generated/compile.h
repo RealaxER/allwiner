dba4622226517299487992a13ce431366861686a
jason <buihien29112002@gmail.com>
2024-06-11 21:26:22 +0700

    [MODIFY] build

---
+++ kbuild/4.9.84/i2m/configs/nvr/011A/glibc/8.2.1/spinand/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 184 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#184 SMP PREEMPT Sun Jun 9 23:51:42 +07 2024"
+#define LINUX_COMPILE_BY "bhien"
+#define LINUX_COMPILE_HOST "asus"
+#define LINUX_COMPILER "gcc version 8.2.1 20180802 (GNU Toolchain for the A-profile Architecture 8.2-2018-08 (arm-rel-8.23)) "
